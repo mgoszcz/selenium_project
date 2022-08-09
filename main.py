7904f315772a3a36587eac60ee378d090871bb58
MGoszcz <MGoszcz@ra.rockwell.com>
2022-08-09 14:10:10 +0200

    unittest used

--- main.py
+++ main.py
@@ -1,50 +1,57 @@
 """https://www.lambdatest.com/blog/getting-started-with-selenium-python/"""
+import time
+from unittest import TestCase
+
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 from my_selectors import CssSelector
 
-driver = webdriver.Chrome()
+
 
 items_list_css_selector = CssSelector('ul', 'class', 'list-unstyled')
 list_item_css_selector = CssSelector('input', 'name', '{}')
 form_input_css_selector = CssSelector('input', 'id', 'sampletodotext')
 form_add_button = CssSelector('input', 'id', 'addbutton')
 
+class WebTest(TestCase):
 
-def click_first_two_items_1():
-    # driver.find_element(By.NAME, 'li1').click()
-    driver.find_element(By.CSS_SELECTOR, list_item_css_selector.print().format('li1')).click()
-    # driver.find_element(By.XPATH, xpath_selector.format('li1')).click()
-    driver.find_element(By.CSS_SELECTOR, list_item_css_selector.print().format('li2')).click()
-    # driver.find_element(By.XPATH, xpath_selector.format('li2')).click()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.driver = None
 
-def click_first_two_items_2():
-    items_list = driver.find_element(By.CSS_SELECTOR, items_list_css_selector.print())
-    items = items_list.find_elements(By.TAG_NAME, 'li')
-    for item in items[:2]:
-        item.find_element(By.TAG_NAME, 'input').click()
 
-def add_new_item():
-    input = driver.find_element(By.CSS_SELECTOR, form_input_css_selector.print())
-    input.send_keys('Happy Testing at LambdaTest')
-    add_button = driver.find_element(By.CSS_SELECTOR, form_add_button.print())
-    add_button.click()
+    def setUp(self) -> None:
+        self.driver = webdriver.Chrome()
+        self.driver.get('https://lambdatest.github.io/sample-todo-app/')
 
-def verify_item_added():
-    items_list = driver.find_element(By.CSS_SELECTOR, items_list_css_selector.print())
-    items = items_list.find_elements(By.TAG_NAME, 'li')
-    passed = False
-    for item in items:
-        if item.text == 'Happy Testing at LambdaTest':
-            passed = True
-    assert passed
+    def tearDown(self) -> None:
+        self.driver.close()
 
-driver.get('https://lambdatest.github.io/sample-todo-app/')
-# click_first_two_items_1()
-click_first_two_items_2()
-add_new_item()
-verify_item_added()
-driver.close()
-pass
+
+    def verify_item_added(self):
+        items_list = self.driver.find_element(By.CSS_SELECTOR, items_list_css_selector.print())
+        items = items_list.find_elements(By.TAG_NAME, 'li')
+        passed = False
+        for item in items:
+            if item.text == 'Happy Testing at LambdaTest':
+                passed = True
+        assert passed
+
+    def test_click_first_two_items_2(self):
+
+        items_list = self.driver.find_element(By.CSS_SELECTOR, items_list_css_selector.print())
+        items = items_list.find_elements(By.TAG_NAME, 'li')
+        for item in items[:2]:
+            item.find_element(By.TAG_NAME, 'input').click()
+        done_true_elements = self.driver.find_elements(By.CSS_SELECTOR, 'span.done-true')
+        assert len(done_true_elements) == 2
+
+    def test_add_new_item(self):
+        input_object = self.driver.find_element(By.CSS_SELECTOR, form_input_css_selector.print())
+        input_object.send_keys('Happy Testing at LambdaTest')
+        add_button = self.driver.find_element(By.CSS_SELECTOR, form_add_button.print())
+        add_button.click()
+        self.verify_item_added
+
